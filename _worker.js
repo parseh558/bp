561dab258551b364fd24401c1b872f27d0f849ba
parseh558 <parseh558@gmail.com>
2024-10-04 12:34:57 +0330

    Update _worker263.js

--- _worker.js
+++ _worker.js
@@ -1130,7 +1130,7 @@ function generateRemark(index, port, address, cleanIPs, protocol, configType) {
         ? addressType = 'Clean IP'
         : addressType = isDomain(address) ? 'Domain': isIPv4(address) ? 'IPv4' : isIPv6(address) ? 'IPv6' : '';
 
-    return `💥 ${index} - ${protocol}${type} - ${addressType} : ${port}`;
+    return `💥 🇩🇪 ${index} - ${protocol}${type} - ${addressType} : ${port}`;
 }
 
 function isDomain(address) {
